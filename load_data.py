74f720394f69f431a7ac1feed56b0c54c152d5c1
D00264223 <d00264223@student.dkit.ie>
2024-02-23 20:34:11 +0000

    finish load_data.py

--- load_data.py
+++ load_data.py
@@ -1,70 +1,61 @@
-#!/usr/bin/env python3 
- 
-# psycopg library renamed to make shorter 
+#!/usr/bin/env python3  
+
 import psycopg2 as pg 
-# needed to use DictCursor 
 import psycopg2.extras
 import csv
-import pandas as pd
- 
- 
+
+  
 # connect to DB on local server 
-conn = pg.connect(database='postgres', user='postgres', password='125360') 
- 
+conn = pg.connect(database='postgres', user='postgres', password='125360')
+
 # Psycopg2 can handle multiple concurrent queries (and their results). 
-# For this reason each operation must go through a cursor. 
+# For this reason each operation must go through a cursor.
 cur = conn.cursor(cursor_factory=psycopg2.extras.DictCursor) 
-# NOT the same as server-side cursors which we will meet later. 
 
-csv_file = "customers.csv" 
-with open(csv_file, "r") as f:
-    reader = csv.reader(f)
+def load_data(table, csv_file):
+    '''
+    function loads all the records from csv_file to table
 
-    next(reader)
+    Parameters:
+    table (str): a table to be loaded.
+    csv_file (str): a csv file to take data from 
+    '''
 
-    insert_query = 'INSERT INTO customers (customer_id, first_name, last_name, country, phone_number, email_address) \
-        VALUES (%s, %s, %s, %s, %s, %s)'
-    
-    for row in reader:
-        cur.execute(insert_query, row)
+    with open(csv_file, "r") as f:
+        reader = csv.reader(f)
+        
+        column_names = [row for row in reader][0]
+        column_num = len(column_names)
 
+        # Query as a string 
+        insert_query = f"INSERT INTO {table} ({', '.join(column_names)}) VALUES ({', '.join(['%s'] * column_num)}) \
+            ON CONFLICT ({column_names[0]}) DO NOTHING;"
 
-csv_file = "cars.csv" 
-with open(csv_file, "r") as f:
-    reader = csv.reader(f)
+        # go back to the beginning of csv_file
+        f.seek(0)
 
-    next(reader)
+        # skip the header
+        next(reader)
 
-    insert_query = 'INSERT INTO cars (car_id, car_brand, car_model, car_color, manufacture_year) \
-        VALUES (%s, %s, %s, %s, %s)'
-    
-    for row in reader:
-        cur.execute(insert_query, row)
+        # counting the number of records
+        count = 0
 
+        for row in reader:
 
-# Query as a string 
-query = "SELECT * from customers;" 
+            # Execute the query 
+            cur.execute(insert_query, row)
+            count += 1
 
+        print(f'{count} records were successfully uploaded to table {table}')        
+        
+tables = {
+    'customers': 'customers.csv',
+    'cars': 'cars.csv',
+    'orders': 'orders.csv'
+}
 
- 
-# Execute the query 
-cur.execute(query) 
-
-print(cur.rowcount)
-
-
- 
-# # Can see the number of rows returned 
-# print("number of regions: %s" % cur.rowcount) 
- 
-# # Loop over each row 
-# for row in cur: 
- 
-#     # Access by column name in dict 
-#     print("%s. %s" % (row['id'], row['name'])) 
- 
-# optional: rewind the cursor (if we want to use it again) 
-# cur.scroll(0, 'absolute') 
+for table, csv_file in tables.items():
+    load_data(table, csv_file)    
  
 # Close the cursor and connection
 conn.commit() 
